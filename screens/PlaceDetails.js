d13828c89ae4b9898fa845f01dc9c24a05b9a9d3
anaxnyaaa <ananya.pal@iemlabs.com>
2024-03-04 11:41:29 +0530

    added icon

---
+++ screens/PlaceDetails.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function PlaceDetails() {
+    return (
+        <View>
+            <Text>hii</Text>
+        </View>
+        
+    );
+}
+
+export default PlaceDetails;
+
+const styles=StyleSheet.create({
+
+})
